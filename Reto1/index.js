5f4f14287285854d3eb59000cc87101985a6a92b
ValeriaEstradaL <valeria.1701611154@ucaldas.edu.co>
2022-08-16 17:49:31 -0500

    "Se completa el reto1"

--- Reto1/index.js
+++ Reto1/index.js
@@ -1,13 +1,18 @@
-function consultarHoras(horasDisponibles){
-
-
+function consultarHoras() {
+    let horasDisponibles = document.getElementById("horasDisponibles").value;
+    let asignaturas = [2, 3, 4, 2];
+    asignaturas.sort().reverse();
+    let response = [];
+    for (let index = 0; index < asignaturas.length; index++) {
+        console.log(asignaturas);
+        if (horasDisponibles >= asignaturas[index]) {
+            if ( horasDisponibles>=0 && response.length < 2 ) {
+                horasDisponibles-=asignaturas[index];
+                response.push(asignaturas[index]);
+            }
+        }
+    }
+    console.log(response);
+document.getElementById("horasRecomendadas").value= response;
 }
-function consultarAsignaturas(){
-let asignaturas= [{"A":2}, {"B": 3}, {"C":1}]
-return asignaturas;
-}
 
-function crearRespuesta(){
-
-
-}
