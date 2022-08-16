d875d181d267fdc94b7088208c3ab58b6bca9368
ValeriaEstradaL <valeria.1701611154@ucaldas.edu.co>
2022-08-16 16:40:08 -0500

    Creacion repo

---
+++ Reto1/index.js
@@ -0,0 +1,13 @@
+function consultarHoras(horasDisponibles){
+
+
+}
+function consultarAsignaturas(){
+let asignaturas= [{"A":2}, {"B": 3}, {"C":1}]
+return asignaturas;
+}
+
+function crearRespuesta(){
+
+
+}
